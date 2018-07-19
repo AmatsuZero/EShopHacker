6a0c43b20d43f7bd37ccc1b23b274cb32dceb6d6
jzh16s@hotmail.com <Ultraman7>
2018-07-19 17:21:28 +0800

    Initial Commit

---
+++ EShopHacker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EShopHacker
+//
+//  Created by Jiang,Zhenhua on 2018/7/19.
+//  Copyright © 2018年 Daubert. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
